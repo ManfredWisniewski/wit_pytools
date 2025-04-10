44f3b81733ba4d10f1886a0d79e8190ccb293f30
Manfred Wisniewski <manfred@witconsult.de>
2025-04-10 20:45:42 +0200

    debug movefile

--- cinderellasort.py
+++ cinderellasort.py
@@ -157,31 +157,29 @@ def handlefile(file, sourcedir, targetdir, ftype_sort, clean, clean_nocase, conf
                     nfile = maildata[0]+'_'+maildata[1]+'_'+project_name+'_'+maildata[2]+'.msg'
                     nfile = cleanfilestring(nfile, clean, clean_nocase, replacements)
                     bowl = bowldir(nfile, config_object)
-                    log_message(_('Target dir: "{}"').format(targetdir))
-                    log_message(_('Bowl: "{}"').format(bowl))
-                    log_message(_('Filename: "{}"').format(nfile))
-                    target_path = os.path.join(targetdir, bowl.lstrip('/'), nfile)
+                    target_path = os.path.join(targetdir + bowl, nfile)
                     log_message(_('Moving file: to "{}"').format(target_path))
-                    movefile(sourcedir, file, os.path.join(targetdir, bowl.lstrip('/')), nfile, dryrun)
+                    movefile(sourcedir, file, targetdir + bowl, nfile, dryrun)
                 else:
                     print("No mail information available or incomplete data.")
                     nfile = cleanfilestring(file.name, clean, clean_nocase, replacements)
                     bowl = bowldir(nfile, config_object)
-                    log_message(_('Moving file: {} to {}').format(os.path.join(sourcedir, file), os.path.join(targetdir, bowl, nfile)))
-                    movefile(sourcedir, file, targetdir, bowl, nfile, dryrun)
+                    target_path = os.path.join(targetdir + bowl, nfile)
+                    log_message(_('Moving file: {} to "{}"').format(os.path.join(sourcedir, file), target_path))
+                    movefile(sourcedir, file, targetdir + bowl, nfile, dryrun)
             except Exception as e:
                 print(f"Error handling MSG file {file.name}: {e}")
                 # Fallback to using the original filename
                 nfile = cleanfilestring(file.name, clean, clean_nocase, replacements)
                 if not dryrun and filemode == 'win':
                     bowl = bowldir(nfile, config_object)
-                    movefile(sourcedir, file, targetdir, bowl, nfile, dryrun)
+                    movefile(sourcedir, file, targetdir + bowl, nfile, dryrun)
             else:
                 # Default behavior for other file types
                 nfile = cleanfilestring(file.name, clean, clean_nocase, replacements)
                 if not dryrun and filemode == 'win':
                     bowl = bowldir(nfile, config_object)
-                    movefile(sourcedir, file, targetdir, bowl, nfile, dryrun)
+                    movefile(sourcedir, file, targetdir + bowl, nfile, dryrun)
         
         # File has been handled, so we can break the loop
         break
