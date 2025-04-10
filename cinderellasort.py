19b2e5c74cfb494af5e0e288faf886dc8d1790c6
Manfred Wisniewski <manfred@witconsult.de>
2025-04-10 20:34:35 +0200

    debug

--- cinderellasort.py
+++ cinderellasort.py
@@ -156,10 +156,8 @@ def handlefile(file, sourcedir, targetdir, ftype_sort, clean, clean_nocase, conf
                     nfile = maildata[0]+'_'+maildata[1]+'_'+project_name+'_'+maildata[2]+'.msg'
                     nfile = cleanfilestring(nfile, clean, clean_nocase, replacements)
                     bowl = bowldir(nfile, config_object)
-                    target_path = os.path.join(targetdir, bowl, nfile)
-                    log_message(_('Target dir: {}').format(targetdir))
-                    log_message(_('Moving file: {} to {}').format(os.path.join(sourcedir, file), target_path))
-                    movefile(sourcedir, file, targetdir, bowl, nfile, dryrun)
+                    log_message(_('Moving file: {} to {}').format(os.path.join(sourcedir, file), os.path.join(targetdir, bowl, nfile)))
+                    movefile(sourcedir, file, os.path.join(targetdir, bowl), nfile, dryrun)
                 else:
                     print("No mail information available or incomplete data.")
                     nfile = cleanfilestring(file.name, clean, clean_nocase, replacements)
