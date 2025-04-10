80cafaa0857f70c510e81e0fd3da792f7a6a885c
Manfred Wisniewski <manfred@witconsult.de>
2025-04-10 18:41:01 +0200

    update logger

--- logger.py
+++ logger.py
@@ -1,5 +1,27 @@
 from eliot import to_file
+from eliot.json import EliotJSONEncoder
 import os
+import json
+from datetime import datetime
+
+class CustomEliotEncoder(EliotJSONEncoder):
+    def default(self, obj):
+        result = super().default(obj)
+        if isinstance(obj, datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S')
+        return result
+
+def format_message(message):
+    """Format the Eliot message to our preferences"""
+    # Convert timestamp to readable format
+    if 'timestamp' in message:
+        timestamp = datetime.fromtimestamp(message['timestamp'])
+        message['timestamp'] = timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    
+    # Remove task_uuid
+    message.pop('task_uuid', None)
+    
+    return json.dumps(message, cls=CustomEliotEncoder)
 
 def setup_logging(config):
     """Setup Eliot logging with the given configuration"""
@@ -9,4 +31,4 @@ def setup_logging(config):
     logdir = config['WIT PYTOOLS'].get('nctoolslogdir')
     if logdir:
         log_file = open(os.path.join(logdir, "mailsort.log"), "a")
-        to_file(log_file)
+        to_file(log_file, format_message)
