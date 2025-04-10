57141c2d0f6c913b0ea68529a97af01fb1183b9a
Manfred Wisniewski <manfred@witconsult.de>
2025-04-10 20:50:19 +0200

    movefile fixed

--- cinderellasort.py
+++ cinderellasort.py
@@ -157,16 +157,13 @@ def handlefile(file, sourcedir, targetdir, ftype_sort, clean, clean_nocase, conf
                     nfile = maildata[0]+'_'+maildata[1]+'_'+project_name+'_'+maildata[2]+'.msg'
                     nfile = cleanfilestring(nfile, clean, clean_nocase, replacements)
                     bowl = bowldir(nfile, config_object)
-                    log_message(_('targetdir "{}"').format(targetdir))
-                    log_message(_('bowl "{}"').format(bowl))
-                    log_message(_('nfile "{}"').format(nfile))
+                    log_message(_('Moving MSG file "{}" to "{}"').format(file.name, bowl))
                     movefile(sourcedir, file, targetdir + bowl, nfile)
                 else:
-                    print("No mail information available or incomplete data.")
+                    log_message("No mail information available or incomplete data.")
                     nfile = cleanfilestring(file.name, clean, clean_nocase, replacements)
                     bowl = bowldir(nfile, config_object)
                     target_path = os.path.join(targetdir + bowl, nfile)
-                    log_message(_('Moving file: {} to "{}"').format(os.path.join(sourcedir, file), target_path))
                     movefile(sourcedir, file, targetdir + bowl, nfile, dryrun)
             except Exception as e:
                 print(f"Error handling MSG file {file.name}: {e}")
